545db82d5173eb7a8add3ade79b61b926322cc42
Navishh <navishkanuwan11@gmail.com>
2024-10-02 15:02:21 +0530

    feat:create user model

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,95 @@
+// require("dotenv").config();
+
+// const config = require("./config.json");
+// const mongoose = require("mongoose");
+
+// mongoose.connect(config.connectionString);
+
+// const User = require("./models/user.model");
+
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+
+// const jwt = require("jsonwebtoken");
+// const { authenticateToken } = require("./utilities");
+
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+// app.get("/", (req, res) => {
+//   res.json({ data: "moda whotta" });
+// });
+
+// //Create account
+// app.post("/create-account", async (req, res) => {
+//   const { fullName, email, password } = req.body;
+
+//   if (!fullname) {
+//     return res
+//       .status(400)
+//       .json({ error: true, message: "Full Name is required!" });
+//   }
+
+//   if (!passsword) {
+//     return res
+//       .status(400)
+//       .json({ error: true, message: "Password is required!" });
+//   }
+
+//   if (!email) {
+//     return res
+//       .status(400)
+//       .json({ error: true, message: "Email is required! " });
+//   }
+
+//   if (isUser) {
+//     return res.json({
+//       error: true,
+//       message: "User already exists",
+//     });
+//   }
+
+//   const user = new User({
+//     fullName,
+//     email,
+//     password,
+//   });
+
+//   await user.save();
+
+//   const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+//     expiresIn: "36000m",
+//   });
+// });
+
+// app.listen(8000);
+
+// module.exports = app;
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const User = require("./models/user.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+
+// Middleware to parse incoming JSON
+app.use(express.json()); // This line ensures that req.body is available
 app.use(
   cors({
     origin: "*",
@@ -12,6 +100,51 @@ app.get("/", (req, res) => {
   res.json({ data: "moda whotta" });
 });
 
-app.listen(8000);
+// Create account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required!" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required!" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required!" });
+  }
+
+  const isUser = await User.findOne({ email });
+
+  if (isUser) {
+    return res
+      .status(400)
+      .json({ error: true, message: "User already exists" });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  res.status(201).json({ user, accessToken });
+});
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
 
 module.exports = app;
